867a56797848084445c6ffca8fc58859755472a2
kyeonghun.seo@day2life.com <kyeonghun.seo@day2life.com>
2018-01-02 16:37:29 +0900

    Committed by Kyeonghun Seo

--- hellocal/src/main/java/com/hellowo/hellocal/utils/ViewUtil.java
+++ hellocal/src/main/java/com/hellowo/hellocal/utils/ViewUtil.java
@@ -10,9 +10,8 @@ import android.util.TypedValue;
 public class ViewUtil {
 
     public static int dpToPx(float dp, Context context) {
-        int px = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp,
+        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp,
                 context.getResources().getDisplayMetrics());
-        return px;
     }
 
 }
